6ffc291ea69d111e09dc056a9ca693034e3fa061
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-10-15 15:12:47 -0500

    Rueda de Gasto Agregado

--- src/Components/ControlPresupuesto.jsx
+++ src/Components/ControlPresupuesto.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar"
+import "react-circular-progressbar/dist/styles.css"
 
 const ControlPresupuesto = ({presupuesto, gastos}) =>{
     const [totalGastado, setTotalGastado] = useState(0)
     const [disponible, setDisponible] = useState(0)
+    const [porcentaje, setPorcentaje] = useState(0)
 
     const formatearCantidad = (cantidad) => {
         return cantidad.toLocaleString("es-US", {
@@ -14,6 +17,8 @@ const ControlPresupuesto = ({presupuesto, gastos}) =>{
     useEffect(() => {
         const totalGastado = gastos.reduce((total, gasto) => total + gasto.cantidad, 0)
         const disponible = presupuesto - totalGastado
+        const porcentaje = (totalGastado / presupuesto) * 100
+        setPorcentaje(porcentaje)
         setTotalGastado(totalGastado)
         setDisponible(disponible)
     } , [gastos, presupuesto])
@@ -21,7 +26,15 @@ const ControlPresupuesto = ({presupuesto, gastos}) =>{
     return(
         <div className="contenedor contenedor-presupuesto sombra dos-columnas">
             <div>
-                <p>Rueda de presupuesto</p>
+                <CircularProgressbar 
+                    value={porcentaje}
+                    text={`${porcentaje}% Gastado`}
+                    styles={buildStyles({
+                        pathColor: "#3B8F6",
+                        trailColor: "#F5F5F5",
+                        textColor: "#3B8F6"
+                    })}
+                />
             </div>
             <div className="contenido-presupuesto">
                 <p>
